bb5f0f5262a0c7111931877ff02904d15313f32b
jemmygit <minajemmy@gmail.com>
2023-12-05 20:07:23 -0500

    second commit

--- Class1.java
+++ Class1.java
@@ -3,5 +3,9 @@ package GitClass;
 public class Class1 {
     public static void main(String[] args) {
         System.out.println(" Hello World it is Me ");
+        System.out.println(" Hello World it is Me M ");
+        System.out.println(" Hello World it is Me I ");
+        System.out.println(" Hello World it is Me N ");
+        System.out.println(" Hello World it is Me A ");
     }
 }
